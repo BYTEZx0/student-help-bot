999ed8b65d5fa7e1d653fd5e6e2fbc1025f3dcc1
H4K3R13 <arjunmundamani@gmail.com>
2024-04-20 17:24:04 +0530

    updated welcome message

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
-    bot.reply_to(message, "Hi, its me bot")
+    bot.reply_to(message, "Im here to help")
 
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
