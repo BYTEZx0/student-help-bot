95650ed767ec7524b9055ff674ebfc2007f7048c
akshaykkl <akkukkl10@gmail.com>
2024-04-24 22:52:50 +0530

    Reg_noCheck formatted

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 import telebot
 from configuration.config import API_TOKEN
 import utils.logger as logger_save
-from utils.regCheck import regCheck
+from utils.reg_check import reg_check
 from telebot.util import user_link
 import re
 
@@ -55,7 +55,7 @@ def process_reg_no_step(message):
         chat_id = message.chat.id
         reg_no = message.text
         #add condition to check reg_no
-        if not regCheck(reg_no):
+        if not reg_check(reg_no):
             msg = bot.reply_to(message, 'Enter Correct University Number')
             bot.register_next_step_handler(msg, process_reg_no_step)
             return
--- utils/reg_check.py
+++ utils/reg_check.py
@@ -1,6 +1,6 @@
 import re 
 
-def regCheck(reg):
+def reg_check(reg):
     if re.match(r'^(NA)|(na)[0-9]{2}([A-Z]|[a-z]){4}[0-9]{2}$', reg):
         return True
     else:
