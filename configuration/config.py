aaf0f347a897dec0cf4e076cb8ef54d38aafefa6
HARI-G-YADHAV <ambadileelakal@gmail.com>
2024-05-01 22:34:44 +0530

    supa

--- configuration/config.py
+++ configuration/config.py
@@ -11,6 +11,6 @@ parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
 dotenv_path = os.path.join(parent_dir, '.env')
 load_dotenv(dotenv_path)
 
-API_TOKEN = os.getenv("TOKEN")
+API_TOKEN = os.getenv("BOt_TOKEN")
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
