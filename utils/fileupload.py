fbff3229cb76b172ce49b4d571909e059ed043d6
H4K3R13 <arjunmundamani@gmail.com>
2024-05-02 23:57:13 +0530

    changed the file type

--- utils/fileupload.py
+++ utils/fileupload.py
@@ -1,9 +1,13 @@
 import requests
 
-files = {"document":open("./bitcoin.pdf", 'rb')}
+files = {"document":open("../pdf/bitcoin.pdf", 'rb')}
 
 response = requests.post('https://api.telegram.org/bot7084726284:AAEQPkxGuJy8CclMOf79xHOzYWgiD_8uLy0/sendDocument?chat_id=-1002117509264', files=files)
 
+print(type(response))
+
+json_reponse = response.json()
+print(f"{json_reponse} : {type(json_reponse)}")
 #reponse_json = json.loads(response)
 print("reponse", list(response))
 
